6666ef986f6d2bda9d0d1c10856dabceee66b0aa
siwar149 <onyxia@rstudio-867954-0.rstudio-867954.projet-esteem.svc.cluster.local>
2024-03-30 16:09:24 +0000

    Some order imposed to the file system

--- scripts/playing-with-IO.R
+++ scripts/playing-with-IO.R
@@ -7,17 +7,54 @@ Z <- matrix(c(80, 20, 50, 30,
               80, 100, 175, 100,
               40, 120, 90, 140), ncol = 4, byrow = T)
 
-Y <- c(290, 400, 725, 670)  # Yield
+x <- c(290, 400, 725, 670)  # Output
+
+feu <- c(50, 80, 150, 180) # European final demand
+
+frow <- c(60, 100, 120, 100) # Rest of the World final demand
 
 va <- c(80, 40, 380, 340)    # Value added
 
-A <- matrix(ncol = 4, nrow = 4)
-
-for (i in c(1:4)) {A[i,] <- Z[i,] / Y}
+A <- Z %*% solve(diag(x))    # Technical coefficients matrix
 
 L <- solve(diag(4)-A)
 
 L
+
+### let's simulate a demand shock in the first sector of the first country
+
+s <- c(-34, 0, 0, 0)
+
+x1 <- L %*% s 
+
+x + x1
+
+x
+
+
+## Let's see the effect on GOS
+
+gos <- 0.55 * va
+
+goso <- gos / x
+
+# Multiplier of GOS
+
+Sgos <- diag(goso) %*% L
+
+# shock on GOS
+
+kgos <- Sgos %*% s
+
+gos + kgos
+
+gos
+
+
+# start doing it with the big dataset
+###################### Very interesting ##################
+
+
 
 f <- c(12.07, 5.25, 24.14, 18.28) # ghg emissions
 diagf <- diag(f)
@@ -27,13 +64,3 @@ diagf <- diag(f)
 Fghg <- t(L) %*% diagf
 
 Fghg # replicated results from Ceglar, but there is a margin or error
-
-## Toy IUCN matrix
-
-pxt <- matrix(c(1, 1, 1,
-                1, 1, 1,
-                1, 0, 1,
-                0, 2, 1), nrow = 4, byrow = T)
-
-
-t(pxt)
