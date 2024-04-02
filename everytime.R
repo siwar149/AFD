93bd57e6150a8bc1c1f843d591af582d15b863d1
siwar149 <onyxia@rstudio-49330-0.rstudio-49330.projet-esteem.svc.cluster.local>
2024-04-02 12:06:37 +0000

    some minor fixes

--- everytime.R
+++ everytime.R
@@ -8,10 +8,10 @@ lapply(packages, require, character.only=TRUE)
 
 install.packages("paws", repos = "https://cloud.R-project.org")
 
-Sys.setenv("AWS_ACCESS_KEY_ID" = "1T5EDANXNW6GI99NYWAM",
-           "AWS_SECRET_ACCESS_KEY" = "ISoNdUHJ3cDQGQoVECTNxGvaBVFk36X0akaJAKde",
+Sys.setenv("AWS_ACCESS_KEY_ID" = "JABHEOJOWQXHXVALIL78",
+           "AWS_SECRET_ACCESS_KEY" = "ShqKC8gzUWmEB8xdULHMD9kSmR5HFP3ByqIUWvOi",
            "AWS_DEFAULT_REGION" = "us-east-1",
-           "AWS_SESSION_TOKEN" = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiIxVDVFREFOWE5XNkdJOTlOWVdBTSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sImF1ZCI6WyJtaW5pby1kYXRhbm9kZSIsIm9ueXhpYSIsImFjY291bnQiXSwiYXV0aF90aW1lIjoxNzEwODUzNjgyLCJhenAiOiJvbnl4aWEiLCJlbWFpbCI6Im9ydGl6Z3V6bWFuc2FAYWZkLmZyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTcxMjUwNTI0MiwiZmFtaWx5X25hbWUiOiJPcnRpeiIsImdpdmVuX25hbWUiOiJTaXdhciIsImdyb3VwcyI6WyJVU0VSX09OWVhJQSIsImVzdGVlbSJdLCJpYXQiOjE3MTE5MDA0NDEsImlzcyI6Imh0dHBzOi8vYXV0aC5sYWIuc3NwY2xvdWQuZnIvYXV0aC9yZWFsbXMvc3NwY2xvdWQiLCJqdGkiOiI1ODkxMjU3YS0xZTA5LTQ5NzktOTQ4OS1mNzQxMGM4MGQzM2EiLCJuYW1lIjoiU2l3YXIgT3J0aXoiLCJwb2xpY3kiOiJzdHNvbmx5IiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2l3YXIiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtc3NwY2xvdWQiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGdyb3VwcyBlbWFpbCIsInNlc3Npb25fc3RhdGUiOiIzZDg5OTJkNS0zNmU2LTRlZDYtOWVlZC1jNGQ4ZThiYmY5ZjUiLCJzaWQiOiIzZDg5OTJkNS0zNmU2LTRlZDYtOWVlZC1jNGQ4ZThiYmY5ZjUiLCJzdWIiOiIzY2IzYTMzZS0yMzA1LTRkMDEtYjZhYy05ZWFkNzQxYjMwNzEiLCJ0eXAiOiJCZWFyZXIifQ.Q3QY6D8vHclkKgl6GQrKxF1yRB1Eqd5RVUPSKSX77Y_rpRNADZrKm4ugk4ZaMZBtMwkrOjaY6h_CigV65Xnv2Q",
+           "AWS_SESSION_TOKEN" = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NLZXkiOiJKQUJIRU9KT1dRWEhYVkFMSUw3OCIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sImF1ZCI6WyJtaW5pby1kYXRhbm9kZSIsIm9ueXhpYSIsImFjY291bnQiXSwiYXV0aF90aW1lIjoxNzEwODUzNjgyLCJhenAiOiJvbnl4aWEiLCJlbWFpbCI6Im9ydGl6Z3V6bWFuc2FAYWZkLmZyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImV4cCI6MTcxMjY1OTczMiwiZmFtaWx5X25hbWUiOiJPcnRpeiIsImdpdmVuX25hbWUiOiJTaXdhciIsImdyb3VwcyI6WyJVU0VSX09OWVhJQSIsImVzdGVlbSJdLCJpYXQiOjE3MTIwNTQ5MzEsImlzcyI6Imh0dHBzOi8vYXV0aC5sYWIuc3NwY2xvdWQuZnIvYXV0aC9yZWFsbXMvc3NwY2xvdWQiLCJqdGkiOiI0ODEyYzFmMi0zZWMwLTQ4ZjYtOGZiOC1mOGU0MjQzZGI4MmEiLCJuYW1lIjoiU2l3YXIgT3J0aXoiLCJwb2xpY3kiOiJzdHNvbmx5IiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2l3YXIiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImRlZmF1bHQtcm9sZXMtc3NwY2xvdWQiXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGdyb3VwcyBlbWFpbCIsInNlc3Npb25fc3RhdGUiOiIzZDg5OTJkNS0zNmU2LTRlZDYtOWVlZC1jNGQ4ZThiYmY5ZjUiLCJzaWQiOiIzZDg5OTJkNS0zNmU2LTRlZDYtOWVlZC1jNGQ4ZThiYmY5ZjUiLCJzdWIiOiIzY2IzYTMzZS0yMzA1LTRkMDEtYjZhYy05ZWFkNzQxYjMwNzEiLCJ0eXAiOiJCZWFyZXIifQ.60ycZNlIH-_C-xfKzOWOXnEci2LMNyIn91_7sPI9xK-GYkNjpNAuYa6Q4yG04z31FZG4ID2yuV0_C9agVibdFw",
            "AWS_S3_ENDPOINT"= "minio.lab.sspcloud.fr")
 
 library("paws")
@@ -31,12 +31,12 @@ bucketlist(region="") # Check if it's alright
 
 # 0) Loading files 
 
-bucket <- "projet-esteem"
-set_wd <- "Gloria/data/057"
+#bucket <- "projet-esteem"
+#set_wd <- "Gloria/data/057"
 
 # alternative
-bucket <- "siwar"
-set_wd <- "data"
+#bucket <- "siwar"
+#set_wd <- "data"
 
 # Loading matrices (need to upload the .zio files and check dates first)
 #OBJECTNAME <- s3read_using(FUN = data.table::fread,encoding = "UTF-8",
--- scripts/julie scripts/4_score_analysis.R
+++ scripts/julie scripts/4_score_analysis.R
@@ -7,6 +7,8 @@ library(readxl)
 library(ggridges)
 library(mapproj)
 
+bucket = "siwar"
+set_wd <- "data/bio/rds"
 
 ########################################################################
 ##### X - Analyse des scores de Red List ###############################
@@ -30,13 +32,25 @@ summary(score$score)
 # Species analysis
 #######
 
-species <- score_species %>% group_by(taxonid) %>% summarise(score=sum(score_sum)) %>% filter(score>0)
+species <- score_species %>%
+  group_by(taxonid) %>%
+  summarise(score=sum(score_sum)) %>%
+  filter(score>0)
 
 length(species$taxonid) # 4,268 species analysed 
 
-species_info <- readRDS("data/rds/Red_list_species.rds")
+#species_info <- readRDS("/Red_list_species.rds")
 
-species <- species %>% left_join(species_info) %>% group_by(class_name) %>% mutate(n=length(class_name))
+species_info <- s3read_using(FUN = readRDS,
+                    object = paste(set_wd,"/Red_list_species.rds",sep=""),
+                    bucket = bucket, opts = list("region" = ""))
+
+
+species <- species %>%
+  left_join(species_info) %>%
+  group_by(class_name) %>%
+  mutate(n=length(class_name))
+
 
 table(species$class_name) # 2 729 amphibiens ; 1 432 mammifères ; 107 oiseaux 
 
@@ -46,7 +60,11 @@ ggplot(species, aes(x = reorder(class_name, -n))) +
   labs(x="",y="") +
   theme_minimal()
 
-ggplot(species) + geom_density(aes(x=score,fill=class_name)) + facet_wrap(vars(class_name),nrow=3) + theme_ridges() + theme(legend.position = "none")
+ggplot(species) +
+  geom_density(aes(x=score,fill=class_name)) +
+  facet_wrap(vars(class_name),nrow=3) +
+  theme_ridges() +
+  theme(legend.position = "none")
 
 #######
 # World map
@@ -58,8 +76,14 @@ world_map <- map_data("world") %>%
 match_pays_gloria_WM <- read_excel("data/match_pays_gloria_WM.xlsx", 
                                    sheet = "WM_Olson_gloria")
 
-country_score <- score_species %>%  group_by(country) %>% summarize(score=sum(score_sum)) %>% distinct() %>% left_join(match_pays_gloria_WM, by=c("country"="pays_g")) 
-map <- world_map %>% left_join(country_score,by=c("region"="WM"))
+country_score <- score_species %>%
+  group_by(country) %>%
+  summarize(score=sum(score_sum)) %>%
+  distinct() %>%
+  left_join(match_pays_gloria_WM, by=c("country"="pays_g"))
+
+map <- world_map %>%
+  left_join(country_score,by=c("region"="WM"))
 
 map %>%
   ggplot(aes(map_id = region, fill = score)) +
@@ -90,4 +114,5 @@ ggplot(BP, aes(y = score, x = reorder(sector, -med))) +
 # Min/Max normalization
 #######
 
-species <- species %>% mutate(score_scaled = (score - min(score)) / (max(score) - min(score))) # min / max normalization de toute la colonne score
+species <- species %>%
+  mutate(score_scaled = (score - min(score)) / (max(score) - min(score))) # min / max normalization de toute la colonne score
