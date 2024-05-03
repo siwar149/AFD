cb26c3c504656e246df355164fcb9d06e0af3bf6
siwar149 <onyxia@rstudio-208205-0.rstudio-208205.projet-esteem.svc.cluster.local>
2024-05-03 09:54:23 +0000

    Output of european countries computed

--- scripts/thesis/real_side.R
+++ scripts/thesis/real_side.R
@@ -1,14 +1,13 @@
 #### Real side analysis
 
 library("tidyr")
-install.packages("Matrix")
 
 ##### STAR metric
 
 ### setting bucket and all
 
-bucket1 = "projet-esteem"
-bucket2 = "siwar"
+bucket1 <- "projet-esteem"
+bucket2 <- "siwar"
 
 set_wd1 <- "Gloria/matrices"
 set_wd2 <- "data/Gloria"
@@ -55,11 +54,11 @@ eu <-  c('AUT', 'BEL', 'BGR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST',
 #eu1 <- c("AUT", "BEL", "DEU", "ESP", "FRA", "HRV", "HUN", "ITA",
 #         "LUX", "POL", "PRT", "SVK")
 
-f <- as.data.frame(s3read_using(FUN = data.table::fread,
+f <- as.data.table(s3read_using(FUN = data.table::fread,
                                 object = paste(set_wd1,"/FD_2019.rds",sep=""),
                                 bucket = bucket1, opts = list("region" = "")))
 
-label_f <- as.data.frame(s3read_using(FUN = readRDS,
+label_f <- as.data.table(s3read_using(FUN = readRDS,
                                       object = paste(set_wd2,"/label_FD.rds",sep=""),
                                       bucket = bucket2, opts = list("region" = "")))
 
@@ -79,10 +78,52 @@ View(f[which(apply(f1, 1, function(row) any(row < 0))), index1])
 # Then we exclude changes in inventories from the footprint calculation
 f1 <- as.matrix(f1[,-which(apply(f1, 2, function(col) sum(col < 0) > 0))])
 
+# Define group size
+group_size1 <- 6
+
+# Calculate number of groups
+num_groups1 <- ncol(f) %/% group_size1
+
+# Initialize empty list to store results
+f_sum <- list()
+
+# Loop through each group and calculate row sums
+for (i in 0:(num_groups1 - 1)) {
+  f_sum[[i + 1]] <- f[, rowSums(.SD, na.rm = TRUE), 
+                            .SDcols = ((i * group_size1 + 1):((i + 1) * group_size1))]
+}
+
+# Combine results into a single data.table
+f_sum <- as.data.table(do.call(cbind, f_sum))
+
+# Saving the final
+s3write_using(x = as.data.table(f_sum), FUN = data.table::fwrite, na = "", 
+              object = paste(set_wd2,"/f_sum.rds",sep=""),
+              bucket = bucket2, opts = list("region" = ""))
 
 L <- as.matrix(s3read_using(FUN = data.table::fread,
                             object = paste(set_wd1,"/L_2019.rds",sep=""),
                             bucket = bucket1, opts = list("region" = "")))
+
+f_sum <- as.matrix(f_sum)
+
+# calculate output for European countries
+x_w <- L %*% f_sum
+
+x_w <- as.data.table(x_w)
+
+s3write_using(x = as.data.table(x_w), FUN = data.table::fwrite, na = "", 
+              object = paste(set_wd2,"/x_w.rds",sep=""),
+              bucket = bucket2, opts = list("region" = ""))
+
+
+x_eu <- x_w[, ..which(unique(label_f$V1) %in% eu)]
+
+s3write_using(x = as.data.table(x_eu), FUN = data.table::fwrite, na = "", 
+              object = paste(set_wd2,"/x_eu.rds",sep=""),
+              bucket = bucket2, opts = list("region" = ""))
+
+
 
 
 # calculate the footprint matrix
