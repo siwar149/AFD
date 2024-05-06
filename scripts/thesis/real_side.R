99228f33c287d6bc8d1d1ef5c45d37c8cd347c6f
siwar149 <onyxia@rstudio-208205-0.rstudio-208205.projet-esteem.svc.cluster.local>
2024-05-06 08:50:43 +0000

    Deleted unnecesary lines of code and fixed some calculation issues

--- scripts/thesis/real_side.R
+++ scripts/thesis/real_side.R
@@ -29,7 +29,7 @@ x <- s3read_using(FUN = data.table::fread,
                   object = paste(set_wd1,"/x_2019.rds",sep=""),
                   bucket = bucket1, opts = list("region" = ""))
 
-label_IO <- as.data.frame(s3read_using(FUN = readRDS,
+label_IO <- as.data.table(s3read_using(FUN = readRDS,
                   object = paste(set_wd2,"/label_IO.rds",sep=""),
                   bucket = bucket2, opts = list("region" = "")))
 
@@ -54,13 +54,13 @@ eu <-  c('AUT', 'BEL', 'BGR', 'HRV', 'CYP', 'CZE', 'DNK', 'EST',
 #eu1 <- c("AUT", "BEL", "DEU", "ESP", "FRA", "HRV", "HUN", "ITA",
 #         "LUX", "POL", "PRT", "SVK")
 
-f <- as.data.table(s3read_using(FUN = data.table::fread,
+f <- s3read_using(FUN = data.table::fread,
                                 object = paste(set_wd1,"/FD_2019.rds",sep=""),
-                                bucket = bucket1, opts = list("region" = "")))
+                                bucket = bucket1, opts = list("region" = ""))
 
-label_f <- as.data.table(s3read_using(FUN = readRDS,
+label_f <- s3read_using(FUN = readRDS,
                                       object = paste(set_wd2,"/label_FD.rds",sep=""),
-                                      bucket = bucket2, opts = list("region" = "")))
+                                      bucket = bucket2, opts = list("region" = ""))
 
 f1 <- f[, which(label_f$V1 %in% eu)]
 #f2 <- f[, which(label_f$V1 %in% eu1)]
@@ -78,52 +78,10 @@ View(f[which(apply(f1, 1, function(row) any(row < 0))), index1])
 # Then we exclude changes in inventories from the footprint calculation
 f1 <- as.matrix(f1[,-which(apply(f1, 2, function(col) sum(col < 0) > 0))])
 
-# Define group size
-group_size1 <- 6
-
-# Calculate number of groups
-num_groups1 <- ncol(f) %/% group_size1
-
-# Initialize empty list to store results
-f_sum <- list()
-
-# Loop through each group and calculate row sums
-for (i in 0:(num_groups1 - 1)) {
-  f_sum[[i + 1]] <- f[, rowSums(.SD, na.rm = TRUE), 
-                            .SDcols = ((i * group_size1 + 1):((i + 1) * group_size1))]
-}
-
-# Combine results into a single data.table
-f_sum <- as.data.table(do.call(cbind, f_sum))
-
-# Saving the final
-s3write_using(x = as.data.table(f_sum), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/f_sum.rds",sep=""),
-              bucket = bucket2, opts = list("region" = ""))
 
 L <- as.matrix(s3read_using(FUN = data.table::fread,
                             object = paste(set_wd1,"/L_2019.rds",sep=""),
                             bucket = bucket1, opts = list("region" = "")))
-
-f_sum <- as.matrix(f_sum)
-
-# calculate output for European countries
-x_w <- L %*% f_sum
-
-x_w <- as.data.table(x_w)
-
-s3write_using(x = as.data.table(x_w), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/x_w.rds",sep=""),
-              bucket = bucket2, opts = list("region" = ""))
-
-
-x_eu <- x_w[, ..which(unique(label_f$V1) %in% eu)]
-
-s3write_using(x = as.data.table(x_eu), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/x_eu.rds",sep=""),
-              bucket = bucket2, opts = list("region" = ""))
-
-
 
 
 # calculate the footprint matrix
@@ -131,6 +89,7 @@ plcy <- E %*% L %*% f1
 
 # Convert to data.table
 plcy <- as.data.table(plcy)
+
 
 # Define groups of 5 contiguous columns
 
@@ -173,8 +132,9 @@ plcy_sum_eu_long <- plcy_sum_eu %>%
 top_12_eu <- plcy_sum_eu_long %>%
   group_by(variable) %>%
   top_n(12, value) %>%
-  mutate(sum_value = sum(value)) %>%
-  mutate(share = value / sum_value)
+  mutate(sum_value = sum(value),
+         share = value / sum_value,
+         shock = 0.01)
 
 
 # EXT
@@ -190,10 +150,11 @@ plcy_sum_ext_long <- plcy_sum_ext %>%
 top_12_ext <- plcy_sum_ext_long %>%
   group_by(variable) %>%
   top_n(12, value) %>%
-  mutate(sum_value = sum(value)) %>%
-  mutate(share = value / sum_value)
+  mutate(sum_value = sum(value),
+         share = value / sum_value,
+         shock = 0.01)
 
-rm(plcy_sum_ext_long, plcy_sum_eu_long, plcy_sum_ext, plcy_sum_eu, k, g)
+rm(plcy_sum_ext_long, plcy_sum_eu_long, plcy_sum_ext, plcy_sum_eu)
 
 
 top_12_eu <- top_12_eu %>%
@@ -215,24 +176,24 @@ dplcy <- label_IO %>%
   mutate(region = if_else(is_eu, "eu", "ext"),
          id = paste(sector, region))
 
+# changing shock
 top_12_merge <- top_12 %>%
   mutate(id = paste(sector, region)) %>%
-  select(variable, id, share)
+  select(variable, id, shock)
 
 for (i in names(plcy_sum)) {
   dplcy <- dplcy %>%
-    left_join(top_12[top_12$variable == i,], by = "id") %>%
+    left_join(top_12_merge[top_12_merge$variable == i,], by = "id") %>%
     select(-variable) %>%
-    rename(!!i := share)
+    rename(!!i := shock)
 }
 
 
 dplcy[, 6:33] <- dplcy[, 6:33] * -1
 
-dplcy <- replace(dplcy, is.na(dplcy), 0)
-
 dplcy <- dplcy[, 6:33]
   
+
 
 s3write_using(x = as.data.table(dplcy), FUN = data.table::fwrite, na = "", 
               object = paste(set_wd2,"/dplcy.rds",sep=""),
@@ -275,173 +236,60 @@ s3write_using(x = as.data.table(dx), FUN = data.table::fwrite, na = "",
               bucket = bucket2, opts = list("region" = ""))
 
 
-###############################################
-score_ext <- score[which(!score$iso %in% eu),]
-score_eu <- score[which(score$iso %in% eu),]
+dx <- s3read_using(FUN = data.table::fread,
+                   object = paste(set_wd2,"/dx.rds",sep=""),
+                   bucket = bucket2, opts = list("region" = ""))
 
-non_eu <- unique(score_ext$iso)
+dx <- as.data.table(rowSums(dx))
 
 
-nace <- read_excel("data/NACE-Gloria.xlsx", sheet = "Feuil1")
+class(x)
 
-rs_pressure <- rs_pressure %>%
-  left_join(nace, by = c("sector"="Gloria"))
+g <- cbind(dx,x)
 
-score1_ext <- rs_pressure[which(!rs_pressure$iso %in% eu),]
-score1_eu <- rs_pressure[which(rs_pressure$iso %in% eu),]
+g <- cbind(label_IO, g)
 
-### At pressure level
 
-cSectors1_ext <- score1_ext %>%
-  group_by(iso, country, sector, Lfd_Nr) %>%
-  summarise(star = sum(score_sum)) %>%
-  group_by(iso, country) %>%
-  top_n(2, wt = star) %>%
-  arrange(desc(star))
+g <- g[which(g$V1 %in% eu),]
 
-country_sum <- score1_ext %>%
-  group_by(iso, country) %>%
-  summarise(total_star = sum(score_sum))
-
-cSectors1_ext <- cSectors1_ext %>%
-  left_join(country_sum, by = c("iso", "country")) %>%
-  mutate(share = star / total_star)
-
-s3write_using(x = as.data.frame(cSectors1_ext), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/cSectors_ext.rds",sep=""),
+s3write_using(x = as.data.table(g), FUN = data.table::fwrite, na = "", 
+              object = paste(set_wd2,"/g_2019.rds",sep=""),
               bucket = bucket2, opts = list("region" = ""))
 
-### at sector level of NACE
-country_sum <- score1_ext %>%
-  group_by(iso, country, NACE) %>%
-  summarise(total_star = sum(score_sum)) %>%
-  arrange(desc(total_star)) %>%
-  slice_head(n = 1)
-
-
-
-cSectors1_eu <- score1_eu %>%
-  group_by(iso) %>%
-  arrange(desc(score_sum)) %>%
-  slice_head(n = 12)
-
-
-### At sector level
-cSectors_ext <- score_ext %>%
-  group_by(iso) %>%
-  arrange(desc(score)) %>%
-  slice_head(n = 1)
-
-cSectors_eu <- score_eu %>%
-  group_by(iso) %>%
-  arrange(desc(score)) %>%
-  slice_head(n = 1)
-
-pSectors_ext <- unique(cSectors_ext$sector)
-pSectors_eu <- unique(cSectors_eu$sector)
-
-rm("score")
-
-
-
-### Just keeping the final demand from EU and certain types of demand
-
-td <- c("Household final consumption P.3h", "Government final consumption P.3g")
-
-
-
-### We get only the household consumption from final demand and EU
-
-
-
-
-#f[which(label_IO$V1 %in% eu),] <- 0
-
-f1[which(label_IO$V1 %in% non_eu & !label_IO$V3 %in% pSectors_ext), ] <- 0
-f1[which(label_IO$V1 %in% eu & !label_IO$V3 %in% pSectors_eu), ] <- 0
-
-s <- f1
-
-rm("f1")
-
-
-s3write_using(x = as.data.frame(s), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/s1_2019.rds",sep=""),
-              bucket = bucket2, opts = list("region" = ""))
-
-
-### check final output
-
-x <- s3read_using(FUN = data.table::fread,
-                    object = paste(set_wd1,"/x_2019.rds",sep=""),
-                    bucket = bucket1, opts = list("region" = ""))
-
-colnames(x) <- "output"
-
-
-##### Now simulate demand #####
-
-L <- as.matrix(s3read_using(FUN = data.table::fread,
-             object = paste(set_wd1,"/L_2019.rds",sep=""),
-             bucket = bucket1, opts = list("region" = "")))
-
-
-s <- as.matrix(s)
-
-
-### reduction of 1% final demand on specific sectors of EU and EXT
-s <- -(s * 0.01)
-
-k <- L %*% s
-
-### Keeping the results for the relevant countries
-
-k <- cbind(k, x)
-
-k <- cbind(label_IO, k)
-
-k <- k[which(k$V1 %in% eu),]
-
-colnames(k)[4] <- "loss"
-
-s3write_using(x = as.data.frame(k), FUN = data.table::fwrite, na = "", 
-              object = paste(set_wd2,"/k_2019.rds",sep=""),
-              bucket = bucket2, opts = list("region" = ""))
+g <- s3read_using(FUN = data.table::fread,
+                   object = paste(set_wd2,"/g_2019.rds",sep=""),
+                   bucket = bucket2, opts = list("region" = ""))
 
 
 ### Add a column with the NACE sectors
 nace <- read_excel("data/NACE-Gloria.xlsx", sheet = "Feuil1")
 
-k <- k %>%
+g <- g %>%
   left_join(nace, by = c("V3"="Gloria"))
 
-### Drop anomalous values
 
-k <- k %>%
-  filter(abs(loss) <= output)
+g <- g[, -3]
 
-k <- k[, -3]
-
-k <- k %>%
+g <- g %>%
   group_by(V1, V2, NACE) %>%
   summarise(
-    loss = sum(loss),
-    output = sum(output)
+    dx = sum(dx),
+    x = sum(x)
   )
 
-k <- k %>%
-  mutate(rshare= abs(loss) / output * 100)
+g <- g %>%
+  mutate(rshare= abs(dx) / x * 100)
 
-rm("L")
+#rm("L")
 
-s3write_using(x = as.data.frame(k), FUN = data.table::fwrite, na = "", 
-              object = paste("data/Gloria/k5_2019.rds",sep=""),
+s3write_using(x = as.data.table(g), FUN = data.table::fwrite, na = "", 
+              object = paste("data/Gloria/g1_2019.rds",sep=""),
               bucket = bucket2, opts = list("region" = ""))
 
 
-k <- as.data.frame(s3read_using(FUN = data.table::fread,
-              object = paste(set_wd2,"/k1_2019.rds",sep=""),
-              bucket = bucket2, opts = list("region" = "")))
+#k <- as.data.frame(s3read_using(FUN = data.table::fread,
+#              object = paste(set_wd2,"/k1_2019.rds",sep=""),
+#              bucket = bucket2, opts = list("region" = "")))
 
 
 # Checking bach data
@@ -449,17 +297,19 @@ k <- as.data.frame(s3read_using(FUN = data.table::fread,
 bach <- read.csv("data/export-bach-2019.csv", sep = ";", header = T)
 iso <- read_excel("data/iso.xlsx", sheet = "Sheet1")
 
-k <- k %>%
+g <- g %>%
   left_join(iso, by = c("V1"="iso"))
 
 sample <- unique(bach$country)
 
-k <- k[which(k$eu %in% sample), ]
+g <- g[which(g$eu %in% sample), ]
 
 
-s3write_using(x = as.data.frame(k), FUN = data.table::fwrite, na = "", 
-              object = paste("data/Gloria/k_2019.rds",sep=""),
+s3write_using(x = as.data.table(g), FUN = data.table::fwrite, na = "", 
+              object = paste("data/Gloria/g2_2019.rds",sep=""),
               bucket = bucket2, opts = list("region" = ""))
+
+
 
 
 
