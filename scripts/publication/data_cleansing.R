6165ab46a62bdf0f428ab0cbb3f3044d3ce7de34
siwar149 <onyxia@rstudio-365662-0.rstudio-365662.projet-esteem.svc.cluster.local>
2024-08-12 14:49:35 +0000

    done a lot of progress

--- scripts/publication/data_cleansing.R
+++ scripts/publication/data_cleansing.R
@@ -20,6 +20,7 @@ x <- s3read_using(FUN = data.table::fread,
                   bucket = bucket1, opts = list("region" = ""))
 
 x <- x + 0.0001 # No data on Yemen's output
+x <- as.data.table(x)
 
 # Labels of final demand, output, and satelite accounts
 label_IO <- as.data.table(s3read_using(FUN = readRDS,
@@ -56,6 +57,9 @@ s3write_using(x = as.data.table(G), FUN = data.table::fwrite, na = "",
               object = paste(set_wd2,"/G_2019.rds",sep=""),
               bucket = bucket2, opts = list("region" = ""))
 
+eu1 <- c("AUT", "BEL", "DEU", "ESP", "FRA", "HRV", "HUN", "ITA",
+         "LUX", "POL", "PRT", "SVK")
+
 ## Getting backward and forward induced extinction-risk
 # backward
 
@@ -63,75 +67,42 @@ L <- s3read_using(FUN = data.table::fread,
                   object = paste(set_wd1,"/L_2019.rds",sep=""),
                   bucket = bucket1, opts = list("region" = ""))
 
-bk <- diag(as.integer(as.matrix(e))) %*% as.matrix(L) - diag(as.integer(as.matrix(e)))
+G <- s3read_using(FUN = data.table::fread,
+                  object = paste(set_wd1,"/G_2019.rds",sep=""),
+                  bucket = bucket1, opts = list("region" = ""))
 
-bk <- as.data.table(colSums(bk))
+bk <- t(as.matrix(e)) %*% (as.matrix(L) - diag(dim(L)[1]))
 
-# forward
-fr <- as.matrix(G) %*% diag(as.integer(as.matrix(e))) - diag(as.integer(as.matrix(e)))
+bk <- as.data.table(t(bk))
 
-fr <- as.data.table(rowSums(fr))
-
-eu1 <- c("AUT", "BEL", "DEU", "ESP", "FRA", "HRV", "HUN", "ITA",
-         "LUX", "POL", "PRT", "SVK")
-
+# need to multiply by final demand to get the absolute footprint
+bk <- bk * f
 
 # weird results here
 bk_eu <- cbind(label_IO[which(label_IO$iso %in% eu1),],
                bk[which(label_IO$iso %in% eu1),])
 
-# I got negative values in this one (def not doing something right)
+# forward (I am really not interested on the forward footprint of Europe)
+fr <- (as.matrix(G) - diag(dim(G)[1])) %*% as.matrix(e)
+
+fr <- as.data.table(fr)
+
+fr <- fr * v
+
+# Found the mistake here. Now everything fine
 fr_eu <- cbind(label_IO[which(label_IO$iso %in% eu1),],
                fr[which(label_IO$iso %in% eu1),])
-# Interesting but not useful because I know what sectors I am goint to shock
-rm("bk", "fr", "fr_eu", "bk_eu", "L", "G")
 
-#### Using the Gosh model to measure the impact of halting exports of 
-# agricultural products to Europe
-
-B <- as.data.table(s3read_using(FUN = data.table::fread,
-                  object = paste(set_wd2,"/B_2019.rds",sep=""),
-                  bucket = bucket2, opts = list("region" = "")))
-
-# countries where the shock comes from
-latam6 <- c("HND", "COL", "BRA", "GTM", "PER", "ECU")
-
-nace <- read_excel("data/NACE-Gloria.xlsx", sheet = "Feuil1")
-
-label_IO <- label_IO %>%
-  left_join(nace, by = c("sector"="Gloria"))
-
-in_eu <- which(label_IO$iso %in% eu1)
-
-latam6A <- which(label_IO$iso %in% latam6 & label_IO$NACE == "A")
-
-# Shocking only the exports to EU countries
-B[latam6A, in_eu] <- 0
-
-v <- s3read_using(FUN = data.table::fread,
-                  object = paste(set_wd1,"/VA_2019.rds",sep=""),
-                  bucket = bucket1, opts = list("region" = ""))
-
-v <- as.data.table(colSums(v))
-
-label_VA <- s3read_using(FUN = data.table::fread,
-                  object = paste("Gloria/labels/label_VA.rds",sep=""),
-                  bucket = bucket1, opts = list("region" = ""))
-
-Grj <- diag(dim(B)[1]) - B
-Grj <- solve(Grj)
-Grj <- as.data.table(Grj)
-
-x1 <- t(as.matrix(v)) %*% as.matrix(Grj)
-
-x1 <- as.data.table(t(x1))
-x <- as.data.table(x)
-
-dx <- (x1 - x)/x
-
-dx <- cbind(label_IO[in_eu,], dx[in_eu])
+fr_latam6 <- cbind(label_IO[which(label_IO$iso %in% latam6),],
+               fr[which(label_IO$iso %in% latam6),])
 
 
+# Interesting but not useful because I know what sectors I am going to shock
+rm("bk", "fr", "fr_eu", "bk_eu", "G")
+
+
+
+####### ESTEEM ########
 # Starting to look at exposure
 
 exp <- as.data.table(colSums(Z[latam6A, ..in_eu]))
@@ -147,3 +118,49 @@ exp1 <- exp %>%
 
 head(exp1, 28) %>% print(n = 28)
 
+
+# loss in output
+
+#### Using the Gosh model to measure the impact of halting exports of 
+# agricultural products to Europe
+
+A <- as.data.table(s3read_using(FUN = data.table::fread,
+                                object = paste(set_wd1,"/A_2019.rds",sep=""),
+                                bucket = bucket1, opts = list("region" = "")))
+
+# countries where the shock comes from
+latam6 <- c("HND", "COL", "BRA", "GTM", "PER", "ECU")
+
+nace <- read_excel("data/NACE-Gloria.xlsx", sheet = "Feuil1")
+
+label_IO <- label_IO %>%
+  left_join(nace, by = c("sector"="Gloria"))
+
+in_eu <- which(label_IO$iso %in% eu1)
+
+latam6A <- which(label_IO$iso %in% latam6 & label_IO$NACE == "A")
+
+# Shocking only the exports to EU countries
+A[latam6A, in_eu] <- 0
+
+#v <- s3read_using(FUN = data.table::fread,
+#                  object = paste(set_wd1,"/VA_2019.rds",sep=""),
+#                  bucket = bucket1, opts = list("region" = ""))
+
+#v <- as.data.table(colSums(v))
+
+#label_VA <- s3read_using(FUN = data.table::fread,
+#                         object = paste("Gloria/labels/label_VA.rds",sep=""),
+#                         bucket = bucket1, opts = list("region" = ""))
+
+Lcj <- diag(dim(A)[1]) - A
+Lcj <- solve(Lcj)
+Lcj <- as.data.table(Lcj)
+
+x1 <- as.matrix(Lcj) %*% as.matrix(f)
+
+x1 <- as.data.table(x1)
+
+dx <- (x1 - x)/x
+
+dx <- cbind(label_IO[in_eu,], dx[in_eu])
